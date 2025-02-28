bea757eec666a9fb55f0a4c1cb7aa30966a53873
Kai <dk786161616@gmail.com>
2025-02-28 23:52:34 +0530

    StartPageComplete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import RotatingText from "./animated/RotatingText";
+import FallingText from "./animated/FallingText";
+import { FaFingerprint } from "react-icons/fa";
+import Magnet from "./animated/Magnet";
+import MetaBalls from "./animated/MetaBalls";
+import Aurora from "./animated/Aurora";
 
 const App = () => {
   return (
-    <div className='bg-blue-300'>App</div>
-  )
-}
+    <div className="h-screen  flex flex-col p-4 bg-black">
+      <FallingText
+        text={`Hey There..! Wanna Know Me?`}
+        highlightWords={["Hey", "Know", "?", "components", "simplify"]}
+        highlightClass="highlighted"
+        trigger="hover"
+        backgroundColor="transparent"
+        wireframes={false}
+        gravity={0.56}
+        fontSize="2rem"
+        mouseConstraintStiffness={0.9}
+      />
 
-export default App
+      <MetaBalls
+        color="#29d7f2"
+        cursorBallColor="#ffffff"
+        cursorBallSize={2}
+        ballCount={15}
+        animationSize={30}
+        enableMouseInteraction={true}
+        enableTransparency={true}
+        hoverSmoothness={0.05}
+        clumpFactor={1}
+        speed={0.3}
+      />
+    </div>
+  );
+};
+
+export default App;
